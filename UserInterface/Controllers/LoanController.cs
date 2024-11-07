54aa46d8e388b3f541d0dce7358ce8d19138252c
Wilknis Deyvis <wilknis.oliveira@truework.com.br>
2024-11-07 09:52:18 -0300

    feat: locan controller

---
+++ UserInterface/Controllers/LoanController.cs
@@ -0,0 +1,29 @@
+﻿using Lending.Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserInterface.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        private readonly INewLoanUseCase _newLoanUseCase;
+
+        public LoanController(INewLoanUseCase newLoanUseCase)
+        {
+            _newLoanUseCase = newLoanUseCase;
+        }
+
+        [HttpPost("{copyId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult CreateLoan([FromRoute] Guid copyId)
+        {
+            //GetUserId
+            Guid userId = Guid.NewGuid();
+
+            _newLoanUseCase.Execute(copyId, userId);
+
+            return Ok();
+        }
+    }
+}
--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.Application.Extensions;
+using Lending.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCatalogUseCases();
+builder.Services.AddLendingUseCases();
 
 var app = builder.Build();
 
